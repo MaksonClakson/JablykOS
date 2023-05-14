2b46afee79511be50d14d8ab67a26a03a420233c
MaksonClakson <max1112vas@gmail.com>
2023-05-14 21:36:52 +0300

    Add FAT file system

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,56 +1,45 @@
-org 0x7C00
+org 0x0
 bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
-start:
-	jmp main
 
-; Prints a string  to the screen
+start:
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
+;
+; Prints a string to the screen
 ; Params:
 ;   - ds:si points to string
 ;
-
 puts:
-	; save registers we will modify
-	push si
-	push ax
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
 
 .loop:
-	lodsb			; loads next char into al
-	or al, al 		; is next char is null (ZF will be set if result is zero)
-	jz .done		; jump if ZF is set
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
 
-	mov ah, 0x0e
-	mov bh, 0
-	int 0x10
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
 
-	jmp .loop
+    jmp .loop
 
 .done:
-	pop ax
-	pop si
-	ret
+    pop bx
+    pop ax
+    pop si    
+    ret
 
-main:
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-
-	; setup stack
-	mov ss, ax
-	mov sp, 0x7C00   ; stack grows downwards from where we are loaded in memory
-
-	mov si, msg_hello
-	call puts
-
-	hlt   ; stops CPU from executing
-
-
-.halt:
-	jmp .halt
-
-msg_hello: db 'Hello world22!', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
